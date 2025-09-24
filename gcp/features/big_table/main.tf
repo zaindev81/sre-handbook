9e6d6d60a2a3a9f8ee48e3d682d6bb8967caa262
zaindev81 <zaindev81@gmail.com>
2025-09-24 14:59:33 +0700

    big table

---
+++ gcp/features/big_table/main.tf
@@ -0,0 +1,22 @@
+resource "google_bigtable_instance" "main" {
+  name         = "ace-bt-instance"
+  instance_type = "PRODUCTION"            # or "DEVELOPMENT" for non-prod
+  display_name = "ACE Bigtable"
+
+  cluster {
+    cluster_id   = "ace-bt-cluster-1"
+    zone         = var.zone               # e.g. us-central1-a
+    num_nodes    = 3                      # start with 3 nodes
+    storage_type = "SSD"
+  }
+}
+
+# A sample table
+resource "google_bigtable_table" "events" {
+  name          = "events"
+  instance_name = google_bigtable_instance.main.name
+
+  column_family {
+    family = "cf1"
+  }
+}
