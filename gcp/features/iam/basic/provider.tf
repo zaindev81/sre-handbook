858aa3edc85d9ed05d4bf282c9f73c52ac2d67aa
zaindev81 <zaindev81@gmail.com>
2025-10-07 06:27:32 +0700

    feat: add IAM service account and roles configuration

---
+++ gcp/features/iam/basic/provider.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_version = ">= 1.13.3"
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 7.4.0"
+    }
+  }
+}
+
+provider "google" {
+  project = var.project_id
+  region  = var.region
+  zone    = var.zone
+}
+
+module "root_project" {
+  source = "../../"
+
+  gcp_project_id   = var.project_id
+  gcp_region       = var.region
+  gcp_zone         = var.zone
+  gcp_project_name = "iam-basic"
+  environment      = var.environment
+}
